811d7e7e13aa75b6320b2b28790d28833758b68e
benjanaranjo <benja_naranjo@hotmail.com>
2019-12-12 15:28:02 +0100

    instalacion de librerias de testing jest jest-extended y supertest, creacion de dumb test

---
+++ tests/server.test.js
@@ -0,0 +1,17 @@
+//describe sirve para agrupar un conjunto de casos de prueba
+//se estructuran siempre con llamadas de callback
+describe ("Hello World Test", () =>{
+
+    //se describe cada "caso de prueba" dentro del callback de la funcion it,
+    it ("Should do a test",() => {
+        //codigo que se va a testear
+        const a=5;
+        const b=3;
+        const sum = a + b;
+
+        //prueba en la que se define el valor que normalmente esperamos del codigo a probar
+        expect(sum).tobe(8);
+
+    });
+
+});
